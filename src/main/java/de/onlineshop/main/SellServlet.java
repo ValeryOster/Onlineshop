4be2ee613d06eec39964c4ad805517bb6cb98e84
Valery Oster <oster_valera@hotmail.com>
2018-01-03 17:57:40 +0100

    Update SellServlet.java

--- src/main/java/de/onlineshop/main/SellServlet.java
+++ src/main/java/de/onlineshop/main/SellServlet.java
@@ -7,7 +7,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 
 /**
@@ -17,18 +21,52 @@ import java.io.PrintWriter;
  * @version 1.0
  */
 @WebServlet("/sell")
-@MultipartConfig
+@MultipartConfig(
+        maxFileSize = 1024*1024*10,
+        maxRequestSize = 1024*1024*30,
+        location = "/tmp",
+        fileSizeThreshold = 1024*1024)
 public class SellServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+        final String title = request.getParameter("title");
+        final String description = request.getParameter("description");
+        final String price = request.getParameter("price");
         final Part part = request.getPart("foto");
+        OutputStream os = null;
+        InputStream is = null;
+
+        try {
+            String path = "/tmp/"
+                    + part.getSubmittedFileName();
+            File file = new File(path);
+            os = new FileOutputStream(file);
+            byte[] b = new byte[1024];
+            int i = 0;
+            is = part.getInputStream();
+            while ((i = is.read(b)) != -1) {
+                os.write(b, 0, i);
+            }
+        } catch (Exception ex) {
+            throw new ServletException(
+                    ex.getMessage());
+        } finally {
+            os.close();
+            is.close();
+        }
+
         final PrintWriter out = response.getWriter();
-        out.println("<!DOCTYPE HTML>");
-        out.println("Content-Type: " + part.getContentType() + "<br>");
-        out.println("Size: " + part.getSize() + "<br>");
-        out.println("Name: " + part.getName() + "<br>");
-        out.println("Filename: " + part.getSubmittedFileName() + "<br>");
+
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<br>Hochgeladener Artikel: " + title);
+        out.println("<br>Beschreibung: " + description);
+        out.println("<br>Preis: " + price);
+        out.println("<br>Bild: " + part.getSubmittedFileName());
+        out.println("</body>");
+        out.println("</html>");
     }
 }
