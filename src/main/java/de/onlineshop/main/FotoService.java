3bf36a1a53cb89157b5045c4203d65fc6545845c
Valery Oster <oster_valera@hotmail.com>
2018-01-04 17:37:06 +0100

    Add files via upload

---
+++ src/main/java/de/onlineshop/main/FotoService.java
@@ -0,0 +1,35 @@
+package de.onlineshop.main.service;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class FotoService implements Runnable {
+    private InputStream is;
+    private OutputStream os;
+
+    public FotoService(
+            InputStream is,
+            OutputStream os) {
+        this.is = is;
+        this.os = os;
+    }
+
+    public void run() {
+        try {
+            byte[] b = new byte[1024];
+            int i = 0;
+            while ((i = is.read(b)) != -1) {
+                os.write(b, 0, i);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                os.close();
+                is.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
